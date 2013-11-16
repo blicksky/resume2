59c9d4ebd834f701a544388ceddf17dcd3de1671
Andrew Blick <andrew@blicksky.com>
2013-11-16 14:32:26 -0500

    beginning of resume coming together

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 (function(){
 	var fs = require('fs');
 	
-	var templateString = fs.readFileSync('src/resume-template.handlebars', {encoding: 'utf8'});
+	var templateString = fs.readFileSync('src/resume-template.hbs', {encoding: 'utf8'});
 	var data = JSON.parse( fs.readFileSync('src/resume-data.json', {encoding: 'utf8'}) );
 	
 	Handlebars = require('handlebars');
