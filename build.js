ae45a6f71e9e2beb28307126b25aea4243e0fb68
Andrew Blick <andrew@blicksky.com>
2013-11-17 15:10:05 -0500

    solved print layout issue and various other things

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
 (function(){
 	
 	var fs = require('fs');
+
+	var copy = function(from, to) {
+		fs.createReadStream(from).pipe(fs.createWriteStream(to));
+	}
 	
 	var templateString = fs.readFileSync('src/resume-template.hbs', {encoding: 'utf8'});
 	var data = JSON.parse( fs.readFileSync('src/resume-data.json', {encoding: 'utf8'}) );
@@ -9,6 +13,12 @@
 
 	var template = Handlebars.compile( templateString );
 	
-	fs.writeFileSync( 'out.html', template( data ) );
+	if( !fs.existsSync('build') ) {
+		fs.mkdirSync('build');
+	}
+
+	fs.writeFileSync( 'build/resume.html', template( data ) );
+	copy('src/styles.css', 'build/styles.css');
+	copy('src/styles-print.css', 'build/styles-print.css');
 	
 })();
